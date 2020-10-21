167d4ffd176cbe8ea0902e130c9b2a236ad1cfcb
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2020-10-21 19:46:28 +0530

    :wrench: fixed small bugs

--- food.py
+++ food.py
@@ -5,8 +5,8 @@ from constants import DISPLAY_WIDTH, DISPLAY_HEIGHT, PIXEL_SIZE, RED
 class Food:
 
     def __init__(self):
-        self.x = round(random.randrange(1, DISPLAY_WIDTH - PIXEL_SIZE) / 20.0) * 20
-        self.y = round(random.randrange(1, DISPLAY_HEIGHT - PIXEL_SIZE) / 20.0) * 20
+        self.x = round(random.randrange(0, DISPLAY_WIDTH - PIXEL_SIZE) / 20.0) * 20
+        self.y = round(random.randrange(0, DISPLAY_HEIGHT - PIXEL_SIZE) / 20.0) * 20
 
     def draw(self, game_display):
         pygame.draw.rect(game_display, RED, [self.x, self.y, PIXEL_SIZE, PIXEL_SIZE])
